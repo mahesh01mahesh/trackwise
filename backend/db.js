49ea5374cd81d2b8c97dea8ce8ec5af4a7d4bc37
Mahesh <vtu25250@veltech.edu.in>
2026-03-17 14:25:26 +0530

    update db config

--- backend/db.js
+++ backend/db.js
@@ -1,18 +1,19 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root123",
-    database: "trackwise_project"
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE
 });
 
 db.connect((err) => {
-    if (err) {
-        console.log("Database connection failed:", err);
-    } else {
-        console.log("Connected to MySQL database");
-    }
+  if (err) {
+    console.error("Database connection failed:", err);
+  } else {
+    console.log("MySQL Connected");
+  }
 });
 
 module.exports = db;
